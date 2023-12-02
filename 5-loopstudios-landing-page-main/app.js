e3938011bbc4f8c909b2bf7d66811612519ff26c
DevHeris <badairoinioluwa578@gmail.com>
2023-12-02 02:09:13 +0200

    -Minor Fixes in the navigation li when hovered -Added a JS functionality to toggle the position of the header logo and hamburger menu between 'fixed' and 'absolute'

--- 5-loopstudios-landing-page-main/app.js
+++ 5-loopstudios-landing-page-main/app.js
@@ -1,14 +1,19 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.getElementById("primary-navigation");
+const headerLogo = document.querySelector(".hero-container .logo");
 
 const toggleNav = () => {
   const visibility = primaryNav.getAttribute("data-visible");
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
+    navToggle.style.position = "fixed";
+    headerLogo.style.position = "fixed";
   } else {
     primaryNav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
+    navToggle.style.position = "absolute";
+    headerLogo.style.position = "absolute";
   }
 };
 
