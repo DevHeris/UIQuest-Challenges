b0a9b5893710c0615fb4132dc7a0acdf159e1738
DevHeris <badairoinioluwa578@gmail.com>
2023-12-03 12:23:33 +0200

    minor fixes

--- 6-social-media-dashboard-with-theme-switcher-master/app.js
+++ 6-social-media-dashboard-with-theme-switcher-master/app.js
@@ -1,35 +1,4 @@
 const switchInput = document.getElementById("themeSwitch");
-
-// let style = localStorage.getItem("style");
-
-// if (style == null) {
-//   setTheme("light");
-// } else {
-//   setTheme(style);
-// }
-
-// function setTheme(theme) {
-//   if (theme == "light") {
-//     document.getElementById("switcher-id").href = "./themes/light.css";
-//   } else if (theme == "dark") {
-//     document.getElementById("switcher-id").href = "./themes/dark.css";
-//   }
-
-//   localStorage.setItem("style", theme);
-// }
-
-// switchInput.addEventListener("click", () => {
-//   if (style == null) {
-//     setTheme("light");
-//   } else {
-//     setTheme(style);
-//   }
-// });
-
-// document.addEventListener('DOMContentLoaded',()=>{
-
-// })
-
 switchInput.addEventListener("click", () => {
   let isChecked = switchInput.getAttribute("checked");
   if (isChecked === "true") {
@@ -42,8 +11,6 @@ switchInput.addEventListener("click", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  // document.querySelector(" input:checked+.slider:before").style =
-  //   "transform: translateX(145%)";
   let isChecked = switchInput.getAttribute("checked");
   if (isChecked === true) {
     switchInput.setAttribute("checked", true);
