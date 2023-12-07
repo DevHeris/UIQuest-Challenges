78e6b723a55888960188483590c620ecdcac859f
DevHeris <badairoinioluwa578@gmail.com>
2023-12-07 03:11:20 +0200

    Refactored some functions and made sure the lightbox gallery is now fully functional

--- 7-ecommerce-product-page-main/app.js
+++ 7-ecommerce-product-page-main/app.js
@@ -2,6 +2,7 @@ const largeProductImage = document.getElementById("large-product-image");
 const lightboxContainer = document.getElementById("lightbox-gallery-popup");
 const closeBtn = document.querySelector(".close-popup");
 const primaryNav = document.querySelector(".primary-navigation");
+const lightBoxThumbnails = document.querySelectorAll("#lightbox-thumbnail");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const slides = document.querySelectorAll(".slide");
 const prevSlide = document.querySelector(".btn-prev");
@@ -28,32 +29,67 @@ const toggleNav = () => {
 // ==================================
 let curSlide = 0;
 let maxSlide = slides.length - 1;
+const SLIDE_WIDTH_PERCENT = 150;
 
 slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 150}%)`;
+  slide.style.transform = `translateX(${indx * SLIDE_WIDTH_PERCENT}%)`;
 });
 
-nextSlide.addEventListener("click", function () {
+// Function to update the slide and handle "active" class for thumbnails
+const updateSlideAndThumbnails = () => {
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${
+      SLIDE_WIDTH_PERCENT * (indx - curSlide)
+    }%)`;
+
+    lightBoxThumbnails.forEach((thumbnail, index) => {
+      if (thumbnail.classList.contains("active")) {
+        thumbnail.classList.remove("active");
+      }
+
+      if (index === curSlide) {
+        thumbnail.classList.add("active");
+      }
+    });
+  });
+};
+
+// Event delegation for thumbnail clicks
+document
+  .querySelector(".thumbnail-images")
+  .addEventListener("click", (event) => {
+    const clickedThumbnail = event.target.closest("#lightbox-thumbnail");
+
+    if (clickedThumbnail) {
+      const newSrc = clickedThumbnail.querySelector("img").dataset.link;
+
+      // Update the current slide index
+      curSlide = Array.from(lightBoxThumbnails).indexOf(clickedThumbnail);
+
+      // Update the current slide image source
+      slides[curSlide].querySelector("img").src = `${newSrc}.jpg`;
+
+      // Update the slide and thumbnails
+      updateSlideAndThumbnails();
+    }
+  });
+
+nextSlide.addEventListener("click", () => {
   if (curSlide === maxSlide) {
     curSlide = 0;
   } else {
     curSlide++;
   }
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${150 * (indx - curSlide)}%)`;
-  });
+  updateSlideAndThumbnails();
 });
 
-prevSlide.addEventListener("click", function () {
+prevSlide.addEventListener("click", () => {
   if (curSlide === 0) {
     curSlide = maxSlide;
   } else {
     curSlide--;
   }
-
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${150 * (indx - curSlide)}%)`;
-  });
+  updateSlideAndThumbnails();
 });
 
 // =================================================================
@@ -73,9 +109,11 @@ closeBtn.addEventListener("click", () => {
 // ========================================================================
 const updatelargeImage = (event) => {
   const largeImg = document.querySelector(".large-product");
-  const clickedImg = event.currentTarget.firstElementChild;
-  const newSrc = clickedImg.dataset.link;
-  largeImg.src = `${newSrc}.jpg`;
+  const {
+    dataset: { link },
+  } = event.currentTarget.firstElementChild;
+
+  largeImg.src = `${link}.jpg`;
 
   thumbnails.forEach((thumbnail) => {
     if (thumbnail.classList.contains("active")) {
@@ -83,7 +121,7 @@ const updatelargeImage = (event) => {
     }
   });
 
-  clickedImg.parentElement.classList.add("active");
+  event.currentTarget.classList.add("active");
 };
 
 thumbnails.forEach((img) => {
