7e715e383468d335ab41857653036b178906c19b
DevHeris <badairoinioluwa578@gmail.com>
2023-12-08 03:05:06 +0200

    This is the commented version of app.js if you dont understand a particular part

--- 7-ecommerce-product-page-main/app.js
+++ 7-ecommerce-product-page-main/app.js
@@ -1,4 +1,3 @@
-// Selecting elements from the DOM
 const lightBoxThumbnails = document.querySelectorAll(".lightbox-thumbnail");
 const lightboxContainer = document.getElementById("lightbox-gallery-popup");
 const largeProductImage = document.getElementById("large-product-image");
@@ -17,88 +16,71 @@ const itemCountEl = document.querySelector(".item-count");
 
 // Function to toggle the visibility of the primary navigation on mobile
 const toggleNav = () => {
-  const visibility = primaryNav.getAttribute("data-visible");
-  if (visibility === "false") {
-    primaryNav.setAttribute("data-visible", true);
-    navToggle.setAttribute("aria-expanded", true);
-  } else if (visibility === "true") {
-    primaryNav.setAttribute("data-visible", false);
-    navToggle.setAttribute("aria-expanded", false);
-  }
+  const isVisible = primaryNav.getAttribute("data-visible") === "true";
+  primaryNav.setAttribute("data-visible", isVisible ? "false" : "true");
+  navToggle.setAttribute("aria-expanded", isVisible ? "false" : "true");
 };
 
 // Initializing variables for slide navigation
 let curSlide = 0;
 let maxSlide = slides.length - 1;
-const SLIDE_WIDTH_PERCENT = 150;
+let SLIDE_WIDTH_PERCENTAGE = 150;
 
-// Setting initial positions for each slide
-slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * SLIDE_WIDTH_PERCENT}%)`;
-});
+const setInitialSlidePositions = () => {
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * SLIDE_WIDTH_PERCENTAGE}%)`;
+  });
+};
 
 // Function to update the slide and handle "active" class for thumbnails
 const updateSlideAndThumbnails = () => {
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${
-      SLIDE_WIDTH_PERCENT * (indx - curSlide)
+      SLIDE_WIDTH_PERCENTAGE * (indx - curSlide)
     }%)`;
 
-    lightBoxThumbnails.forEach((thumbnail, index) => {
-      if (thumbnail.classList.contains("active")) {
-        thumbnail.classList.remove("active");
-      }
-      if (index === curSlide) {
-        thumbnail.classList.add("active");
-      }
-    });
+    lightBoxThumbnails[indx].classList.toggle("active", indx === curSlide);
   });
 };
 
 // Event listeners for next and previous slide buttons
 nextSlide.addEventListener("click", () => {
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
+  curSlide = (curSlide + 1) % slides.length;
   updateSlideAndThumbnails();
 });
 
 prevSlide.addEventListener("click", () => {
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
+  curSlide = (curSlide - 1 + slides.length) % slides.length;
   updateSlideAndThumbnails();
 });
 
 // Function to update the large product image based on the selected thumbnail
 const updatelargeImage = (event) => {
   const largeImg = document.querySelector(".large-product");
-  const {
-    dataset: { link },
-  } = event.currentTarget.firstElementChild;
+  const clickedThumbnail = event.currentTarget;
+
+  const { link } = clickedThumbnail.firstElementChild.dataset;
 
   largeImg.src = `${link}.jpg`;
 
+  // Remove "active" class from all thumbnails
   thumbnails.forEach((thumbnail) => {
-    if (thumbnail.classList.contains("active")) {
-      thumbnail.classList.remove("active");
-    }
+    thumbnail.classList.remove("active");
   });
 
-  event.currentTarget.classList.add("active");
+  // Add "active" class to the clicked thumbnail
+  clickedThumbnail.classList.add("active");
 };
 
 // Event delegation for lighbox thumbnail clicks
 document
   .querySelector(".thumbnail-images")
   .addEventListener("click", (event) => {
+    // Find the closest ancestor with the class "lightbox-thumbnail"
     const clickedThumbnail = event.target.closest(".lightbox-thumbnail");
 
     if (clickedThumbnail) {
+      // Extract the new source from the clicked thumbnail's dataset
       const newSrc = clickedThumbnail.querySelector("img").dataset.link;
 
       // Update the current slide index
@@ -114,43 +96,54 @@ document
 
 // Function to toggle the visibility of the cart box and update cart items
 const toggleCartBoxVisibility = (event) => {
-  if (cartLogo.classList.contains("has-submenu")) {
-    cartLogo.firstElementChild.setAttribute("aria-expanded", true);
-  } else {
-    cartLogo.firstElementChild.setAttribute("aria-expanded", false);
-  }
+  // Toggle the "has-submenu" class on cartLogo
+  toggleSubMenuClass();
 
-  cartLogo.classList.toggle("has-submenu");
+  // Update "aria-expanded" attribute based on the "has-submenu" class
+  updateAriaExpandedAttribute();
 
-  // Delete all cart items on delete icon click
+  // Handle cart actions (delete and checkout) if not a cart-icon click
   if (!event.target.classList.contains("cart-icon")) {
-    const isDeleteBtn = event.target.alt === "delete from cart";
-    const isCheckoutBtn = event.target.classList.contains("checkout-btn");
-
-    const cartContentEl = document.querySelector(".change");
-    if (isDeleteBtn) {
-      if (confirm("This will delete all items in the cart.")) {
-        cartContentEl.classList.add("empty");
-        cartContentEl.classList.remove("cart-content");
-        cartContentEl.innerHTML = `<p>Your cart is empty.</p>`;
-
-        chosenQuantity = 0;
-        itemCountEl.textContent = chosenQuantity;
-        document.querySelector(".item-count").classList.remove("notEmpty");
-      }
-    } else if (isCheckoutBtn) {
-      cartContentEl.classList.add("empty");
-      cartContentEl.classList.remove("cart-content");
-      cartContentEl.innerHTML = `<p>Your cart is empty.</p>`;
-
-      chosenQuantity = 0;
-      itemCountEl.textContent = chosenQuantity;
-      document.querySelector(".item-count").classList.remove("notEmpty");
-      alert("Checkout Successful!");
-      2;
-    }
+    handleCartActions(event);
   }
 
+  // Set a timeout to add the "has-submenu" class after 5 seconds
+  setSubMenuTimeout();
+};
+
+const toggleSubMenuClass = () => {
+  cartLogo.classList.toggle("has-submenu");
+};
+
+const updateAriaExpandedAttribute = () => {
+  const isSubMenuVisible = cartLogo.classList.contains("has-submenu");
+  cartLogo.firstElementChild.setAttribute("aria-expanded", isSubMenuVisible);
+};
+
+const handleCartActions = (event) => {
+  const isDeleteBtn = event.target.alt === "delete from cart";
+  const isCheckoutBtn = event.target.classList.contains("checkout-btn");
+  const cartContentEl = document.querySelector(".change");
+
+  if (isDeleteBtn && confirm("This will delete all items in the cart.")) {
+    clearCartContent(cartContentEl);
+  } else if (isCheckoutBtn) {
+    clearCartContent(cartContentEl);
+    alert("Checkout Successful!");
+  }
+};
+
+const clearCartContent = (cartContentEl) => {
+  cartContentEl.classList.add("empty");
+  cartContentEl.classList.remove("cart-content");
+  cartContentEl.innerHTML = `<p>Your cart is empty.</p>`;
+
+  chosenQuantity = 0;
+  itemCountEl.textContent = chosenQuantity;
+  document.querySelector(".item-count").classList.remove("notEmpty");
+};
+
+const setSubMenuTimeout = () => {
   const subMenu = document.querySelector(".sub-menu");
 
   // Set a timeout to add the "has-submenu" class after 5 seconds
@@ -162,33 +155,32 @@ const toggleCartBoxVisibility = (event) => {
 };
 
 // Function to check if the viewport is in mobile mode
-function isMobile() {
-  return window.matchMedia("(max-width: 43em)").matches;
-}
-
+const isMobile = () => window.matchMedia("(max-width: 43em)").matches;
 // Initializing quantity-related variables
 let quantity = 0;
 let chosenQuantity = 0;
 
 // Function to update the quantity based on button clicks
 const updateQuantity = (event) => {
-  if (event.target.classList.contains("increase-icon")) {
+  if (
+    event.target.classList.contains("increase-icon") &&
+    quantity < Number.MAX_SAFE_INTEGER
+  ) {
     quantity++;
-    quantityEl.textContent = quantity;
-  } else if (event.target.classList.contains("decrease-icon")) {
-    if (quantity > 0) {
-      quantity--;
-    }
-    quantityEl.textContent = quantity;
+  } else if (event.target.classList.contains("decrease-icon") && quantity > 0) {
+    quantity--;
   }
+
+  quantityEl.textContent = quantity;
   chosenQuantity = quantity;
 };
 
-// Function to update the cart content and reset quantity-related variables
+//Function to update the cart content and reset quantity-related variables
 const updateCart = () => {
   // Check if the chosen quantity is greater than 0
   if (chosenQuantity > 0) {
     const cartContentEl = document.querySelector(".change");
+
     cartContentEl.innerHTML = `
       <div class="shoe-img-container">
         <img src="images/image-product-1-thumbnail.jpg" alt="shoe in cart">
@@ -224,28 +216,28 @@ const updateCart = () => {
 
 // Function to initialize the application
 function initApp() {
-  // Check if the viewport is in mobile mode and show/hide the lightbox accordingly
-  if (isMobile()) {
-    lightboxContainer.classList.remove("hide");
-  } else {
-    lightboxContainer.classList.add("hide");
-  }
+  // Show/hide the lightbox based on the viewport mode
+  lightboxContainer.classList.toggle("hide", !isMobile());
+
+  // Set the initial slide positions
+  setInitialSlidePositions();
 
   // Event listeners for cart, thumbnails, navigation toggle, lightbox, and quantity buttons
   cartLogo.addEventListener("click", toggleCartBoxVisibility);
-  thumbnails.forEach((img) => {
-    img.addEventListener("click", updatelargeImage);
-  });
-
+  thumbnails.forEach((img) => img.addEventListener("click", updatelargeImage));
   navToggle.addEventListener("click", toggleNav);
+
+  // Toggle lightbox visibility on large product image click
   largeProductImage.addEventListener("click", () => {
     lightboxContainer.classList.toggle("hide");
   });
 
+  // Toggle lightbox visibility on close button click
   closeBtn.addEventListener("click", () => {
     lightboxContainer.classList.toggle("hide");
   });
 
+  // Event listener for quantity buttons and add to cart button
   quantityBtns.forEach((btn) => btn.addEventListener("click", updateQuantity));
   addBtn.addEventListener("click", updateCart);
 }
