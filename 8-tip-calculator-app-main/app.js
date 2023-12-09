e9103bfed0143e40f6b64d28518b1ad201d944e5
DevHeris <143361880+DevHeris@users.noreply.github.com>
2023-12-09 21:12:23 +0200

    Update app.js

--- 8-tip-calculator-app-main/app.js
+++ 8-tip-calculator-app-main/app.js
@@ -1,1 +1,36 @@
-const toggleInputElement = () => {};
+document.addEventListener('DOMContentLoaded', function () {
+    const billContainer = document.querySelector('.custom-bill-input-wrapper');
+    const peopleContainer = document.querySelector('.custom-noOfPeople-input-wrapper');
+
+    billContainer.addEventListener('click', function () {
+        replaceWithInput(billContainer, 'billInput');
+    });
+
+    peopleContainer.addEventListener('click', function () {
+        replaceWithInput(peopleContainer, 'numberOfPeopleInput');
+    });
+
+    function replaceWithInput(container, inputId) {
+        const currentValue = container.querySelector('.bill-amount')?.textContent || '0';
+        const inputElement = document.createElement('input');
+        inputElement.type = 'text';
+        inputElement.id = inputId;
+        inputElement.value = currentValue;
+
+        container.replaceWith(inputElement);
+
+        inputElement.addEventListener('blur', function () {
+            const newValue = inputElement.value || '0';
+            container.innerHTML = `<div class="img-wrapper">
+                                      <img src="images/icon-dollar.svg" alt="Dollar Sign">
+                                   </div>
+                                   <p class="bill-amount">${newValue}</p>`;
+
+            container.addEventListener('click', function () {
+                replaceWithInput(container, inputId);
+            });
+        });
+
+        inputElement.focus();
+    }
+});
