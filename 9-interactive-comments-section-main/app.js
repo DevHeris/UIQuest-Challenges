c986a7f586674f8f09c95e639fc48f487433fb43
DevHeris <badairoinioluwa578@gmail.com>
2023-12-11 20:05:12 +0200

    Added for example '@maxblagun' as the default value of the textarea input

--- 9-interactive-comments-section-main/app.js
+++ 9-interactive-comments-section-main/app.js
@@ -1,4 +1,6 @@
 const replyOptions = document.querySelectorAll(".reply");
+const replyTextArea = document.querySelector(".reply-input");
+let replyCount = 0;
 
 const fetchData = async () => {
   const result = await fetch("./data.json");
@@ -8,20 +10,24 @@ const fetchData = async () => {
 };
 
 const toggleReplyBox = (event) => {
+  replyCount++;
   const replyTo = event.currentTarget.closest(".comment-container");
+  const username = replyTo.querySelector(".user-name strong").textContent;
   const replyBox = document.createElement("div");
-  replyBox.classList.add("active-reply", "flex", "white-bg");
 
   replyBox.innerHTML = `
       <div class="profile-picture">
         <img src="images/avatars/image-juliusomo.png" alt="juliusomo profile picture" title="juliusomo">
       </div>
 
-      <textarea class="reply-input" placeholder="Type your reply..."></textarea>
+      <textarea class="reply-input" placeholder="Type your reply...">@${username}</textarea>
       <div>
         <button type="submit" class="reply-btn white-clr btn">Reply</button>
       </div>
   `;
+
+  replyBox.classList.add("active-reply", "flex", "white-bg");
+  replyBox.setAttribute("data-reply", replyCount);
   replyTo.insertAdjacentElement("afterend", replyBox);
 };
 
